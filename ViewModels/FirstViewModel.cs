3c120dec2e0c48362957cf151d8828f428cf593a
FinniV <vechik.korosten@gmail.com>
2018-08-13 09:20:11 +0300

    delete thread code

--- ViewModels/FirstViewModel.cs
+++ ViewModels/FirstViewModel.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using Delegate.Interfaces;
 using Delegate.Models;
 using Delegate.Models.LocalModels;
@@ -10,6 +14,7 @@ namespace Delegate.ViewModels
 {
     public class FirstViewModel : IViewModel
     {
+        public delegate long BinaryOp(int multiply);
         public FirstViewModel()
         {
             ThreadInitializer();
@@ -22,20 +27,46 @@ namespace Delegate.ViewModels
             ViewModelInitialize();
         }
 
-        private void ThreadInitializer()
+        private  void ThreadInitializer()
         {
-            Thread myThread = new Thread(Count);
-            myThread.Start();
+            Count(2);
+            BinaryOp  methodDelegate = Count;
+            IResult ar = methodDelegate.BeginInvoke(2,null,null);
+            while (!ar.IsCompleted)
+            {
+                Console.Write(".");
+                Thread.Sleep(50);
+            }
+            long result = methodDelegate.EndInvoke(ar);
+            Console.WriteLine("Result: " + result);
+
+            Console.ReadLine();
+            // Thread myThread = new Thread(Count(out res));
+            // myThread.Start();
         }
 
-        private void Count(object obj)
+        private long Count(int multiply)
         {
-            for (int i = 1; i < 9; i++)
+            Stopwatch stopWatch = new Stopwatch();
+            System.Console.WriteLine("Count started");
+            stopWatch.Start();
+            var list = new List<int>();
+            long res = 0;
+            for (int i = 0; i < 1000*multiply; i++)
             {
-                Console.WriteLine("Второй поток:");
-                Console.WriteLine(i * i);
-                Thread.Sleep(400);
+                list.Add(i);
             }
+            list.Select(x=> res+=x);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+
+        // Format and display the TimeSpan value.
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+        Console.WriteLine(elapsedTime, "RunTime");
+            System.Console.WriteLine("Count end");
+            return res;
         }
 
         private void ViewModelInitialize()
